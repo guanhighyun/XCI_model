0be88eccc4014181917bf3c8cceff952925a96a0
Kaiyun (Kathy) Guan <kaiyun@live.unc.edu>
2025-09-26 13:45:02 -0700

    .

--- Fig3/Copy_of_Fig3C_lower_panels.m
+++ Fig3/Copy_of_Fig3C_lower_panels.m
@@ -1,51 +1,51 @@
 clear; 
 % Time
-tspan = [0:0.1:1200];
+tspan = [0:0.1:600];
 
 % Error tolerance
-options = odeset('RelTol',1e-8,'AbsTol',repmat(1e-8,[1,13]));
+options = odeset('RelTol',1e-13,'AbsTol',repmat(1e-8,[1,13]));
 
 % Set the value of nuclear volumes
-V_list = [1, 1.3, 1.5, 2];
+V_list = [1, 1.3, 1.46, 2];
 
 for i = 1:numel(V_list)
     V = V_list(i);
     %initial conditions
     act = 0; s1b = 0; x1f = 0; s2b = 0; x2f = 0; s3b = 0; x3f = 0; s4b = 0; x4f = 0;
-    x1b = 1/V; x2b = 2/V; x3b = 10/V; x4b = 20/V;
+    x1b = 1; x2b = 2; x3b = 10; x4b = 20;
     y0 = [act;x1f;x1b;s1b;x2f;x2b;s2b;x3f;x3b;s3b;x4f;x4b;s4b];
     
     % Solve equations
     [t,y] = ode45(@(tt,yy) ODE_model(tt,yy,V),tspan,y0,options);
-    actout = y(:,1)*V;
-    x1fout = y(:,2)*V;
-    x1bout = y(:,3)*V;
-    s1bout = y(:,4)*V;
-    x2fout = y(:,5)*V;
-    x2bout = y(:,6)*V;
-    s2bout = y(:,7)*V;
-    x3fout = y(:,8)*V;
-    x3bout = y(:,9)*V;
-    s3bout = y(:,10)*V;
-    x4fout = y(:,11)*V;
-    x4bout = y(:,12)*V;
-    s4bout = y(:,13)*V;
+    actout = y(:,1);
+    x1fout = y(:,2);
+    x1bout = y(:,3);
+    s1bout = y(:,4);
+    x2fout = y(:,5);
+    x2bout = y(:,6);
+    s2bout = y(:,7);
+    x3fout = y(:,8);
+    x3bout = y(:,9);
+    s3bout = y(:,10);
+    x4fout = y(:,11);
+    x4bout = y(:,12);
+    s4bout = y(:,13);
 
     % Plot results
     figure; plot(t, x1bout, 'LineWidth',3);  hold on; plot(t, x2bout, 'LineWidth',3); plot(t, x3bout, 'LineWidth',3); plot(t, x4bout, 'LineWidth',3); title('Bound Xist'); set(gca,'fontsize',25)
-    title([]); xticks([0:200:600]); xlim([0,600]); ylim([0,100])
+    title([]); xticks([0:200:600]); ylim([0,100])
     set(gca,'fontsize',40)
 end
 
 function dy = ODE_model(t,y,V)
 %Model parameters
-a_act =  1.1/V; % activator synthesis rate from single X chromosome
+a_act =  1.1; % activator synthesis rate from single X chromosome
 d_act = 0.72; % degradation rate of free activator
 
 K_n = 2.2; % concentration of bound SPEN at which activator synthesis rate is half max
 n = 9.4;% Hill coefficient for SPEN supressing activator synthesis rate. 
 
-a_x = 3.2/V; % xist synthesis rate from single X chromosome
+a_x = 3.2; % xist synthesis rate from single X chromosome
 d_x = 0.0076; % degradation rate of Xist 
 
 m = 3.09; % Hill coefficient for bound SPEN reducing dissociation rate Xist
@@ -53,13 +53,13 @@ K_S = 2.06; % concentration of SPEN at which Xist dissociation is half max
 
 K_a = 16.1; % concentration of activator at which Xist transcription is half max
 
-k1 = 0.0084*V; % rate constant for Xist binding to DNA
+k1 = 0.0084/V; % rate constant for Xist binding to DNA
 k2 = 8.7; % maximum dissociation rate for Xist
 k4 = 10.2; % dissoication rate for bound SPEN
-k5 = 0.00026*V; % association rate for SPEN
+k3 = 0.00026/V; % association rate for SPEN
 
-sT = 1000/V;  % total SPEN concentration
-XbsT =  100/V; % concentration of Xist binding sites
+sT = 1000;  % total SPEN concentration
+XbsT =  100; % concentration of Xist binding sites
 
 N_S = round(sT/XbsT); % Number of SPEN that bind to one Xist
 
